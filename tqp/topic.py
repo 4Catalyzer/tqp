406b8e555ea3611e5b352fbdef0b15e6a8a5b5af
Giacomo Tagliabue <giacomo.tag@gmail.com>
2018-03-12 16:08:01 -0400

    fix + improve (#2)

--- tqp/topic.py
+++ tqp/topic.py
@@ -7,8 +7,17 @@ import boto3
 
 class Topic:
     def __init__(self, topic_name):
+        self.topic_name = topic_name
+        self._topic = None
+
+    @property
+    def topic(self):
+        if self._topic is not None:
+            return self._topic
+
         sns = boto3.resource('sns')
-        self.topic = sns.create_topic(Name=topic_name)
+        self._topic = sns.create_topic(Name=self.topic_name)
+        return self._topic
 
     def publish(self, message, dump_json=True, **kwargs):
         if dump_json:
