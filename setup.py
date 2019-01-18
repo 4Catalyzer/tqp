9902a34313947b75038e6aa5026c1c3f15e4f86a
Giacomo Tagliabue <giacomo.tag@gmail.com>
2019-01-18 15:42:29 -0500

    0.2.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def system(command):
 
 setup(
     name="tqp",
-    version="0.2.0",
+    version="0.2.1",
     description="An opinionated library for pub/sub over SQS and SNS",
     url="https://github.com/4Catalyzer/tqp",
     author="Giacomo Tagliabue",
