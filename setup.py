919849787281df7d7b0bdee68fe7b4ede5ad14e0
Jimmy Jia <tesrin@gmail.com>
2019-07-15 09:29:47 -0400

    0.2.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def system(command):
 
 setup(
     name="tqp",
-    version="0.2.1",
+    version="0.2.2",
     description="An opinionated library for pub/sub over SQS and SNS",
     url="https://github.com/4Catalyzer/tqp",
     author="Giacomo Tagliabue",
