bd254370026f324ac36cc54a1c05ade4bf6eba98
Giacomo Tagliabue <giacomo.tag@gmail.com>
2019-09-20 16:09:56 -0400

    add event payload to flask.g (#12)

--- tqp/flask.py
+++ tqp/flask.py
@@ -1,4 +1,34 @@
+from flask import _app_ctx_stack as context_stack
+
 from .topic_queue_poller import TopicQueuePoller
+
+# -----------------------------------------------------------------------------
+
+UNDEFINED = object()
+CTX_PAYLOAD_KEY = 'payload'
+
+# -----------------------------------------------------------------------------
+
+
+def _get_tqp_context():
+    context = context_stack.top
+    if not context:
+        raise RuntimeError("working outside of app context")
+
+    if not hasattr(context, 'tqp'):
+        context.tqp = {}
+
+    return context.tqp
+
+
+def get_ctx_payload():
+    tqp_context = _get_tqp_context()
+
+    if CTX_PAYLOAD_KEY not in tqp_context:
+        raise RuntimeError("working outside of poller handler")
+
+    return tqp_context[CTX_PAYLOAD_KEY]
+
 
 # -----------------------------------------------------------------------------
 
@@ -9,6 +39,7 @@ class FlaskTopicQueuePoller(TopicQueuePoller):
         self.app = app
         self.logger = app.logger
 
-    def handle_message(self, *args, **kwargs):
+    def handle_message(self, msg, payload):
         with self.app.app_context():
-            super().handle_message(*args, **kwargs)
+            _get_tqp_context()[CTX_PAYLOAD_KEY] = payload
+            super().handle_message(msg, payload)
