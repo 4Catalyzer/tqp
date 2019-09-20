566bf086753e18a491ca17a2ead13645956f2a6f
Giacomo Tagliabue <giacomo.tag@gmail.com>
2019-09-20 11:44:56 -0400

    use context

--- tqp/flask.py
+++ tqp/flask.py
@@ -1,5 +1,33 @@
-import flask
+from flask import _request_ctx_stack as context_stack
+
 from .topic_queue_poller import TopicQueuePoller
+
+# -----------------------------------------------------------------------------
+
+UNDEFINED = object()
+CTX_PAYLOAD_KEY = 'payload'
+
+# -----------------------------------------------------------------------------
+
+
+@property
+def _tqp_context():
+    context = context_stack.top
+    if not context:
+        raise RuntimeError("working outside of app context")
+
+    if not hasattr(context, 'tqp'):
+        context.tqp = {}
+
+    return context.tqp
+
+
+@property
+def current_ctx_payload():
+    if CTX_PAYLOAD_KEY not in _tqp_context:
+        raise RuntimeError("working outside of poller handler")
+
+    return _tqp_context[CTX_PAYLOAD_KEY]
 
 # -----------------------------------------------------------------------------
 
@@ -12,5 +40,5 @@ class FlaskTopicQueuePoller(TopicQueuePoller):
 
     def handle_message(self, msg, payload):
         with self.app.app_context():
-            flask.g.tqp_payload = payload
+            _tqp_context[CTX_PAYLOAD_KEY] = payload
             super().handle_message(msg, payload)
