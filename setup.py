94bcb66ea24fec7d280b23bd5182633bf5438aec
Giacomo Tagliabue <giacomo.tag@gmail.com>
2020-11-18 22:57:12 +0100

    0.5.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="tqp",
-    version="0.5.0",
+    version="0.5.1",
     description="An opinionated library for pub/sub over SQS and SNS",
     url="https://github.com/4Catalyzer/tqp",
     author="Giacomo Tagliabue",
